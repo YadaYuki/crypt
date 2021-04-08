03c4ff1e2e85d9c5eba88b06b6c2ba723b02fbc2
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-04-08 18:17:43 +0900

    Add Crypto

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"log"
+	"fmt"
+)
 
-func main() {               
-	fmt.Printf("Hello world")
+var commonIV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+
+func main() {
+	plainText := []byte("My Name Is Yuki Yada")
+
+	keyText := "astaxie12798akljzmknm.ahkjkljl;k" // aes key
+
+	c, err := aes.NewCipher([]byte(keyText))
+	if err != nil {
+		log.Fatal("Error: NewCipher(%d bytes) = %s", len(keyText), err)
+	}
+
+	cfb := cipher.NewCFBEncrypter(c,commonIV)
+	cipherText := make([]byte,len(plainText))
+	cfb.XORKeyStream(cipherText,plainText)
+	fmt.Printf("%s => %x \n",plainText,cipherText)
+
+	cfbdec := cipher.NewCFBDecrypter(c,commonIV)
+	plainTextCopy := make([]byte,len(plainText))
+	cfbdec.XORKeyStream(plainTextCopy,cipherText)
+	fmt.Printf("%x => %s \n",cipherText,plainTextCopy)
 }
